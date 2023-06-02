6139472072f26dc77abdc4088d3153d62bb447cb
dsundarpaul <dsundar0211@gmail.com>
2023-06-02 12:02:31 +0530

    added nav const for side navigation

---
+++ src/navigations/vertical/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import { SettingOutlined } from '@ant-design/icons'
+
+export const SIDENAV = [
+  {
+    title: 'Friends',
+    path: '/friends',
+    icon: ''
+  },
+  {
+    title: 'Events',
+    path: '/events',
+    icon: ''
+  },
+  {
+    title: 'Leaderboard',
+    path: '/leaderboard',
+    icon: ''
+  },
+  {
+    title: 'Community',
+    path: '/community',
+    icon: ''
+  },
+  {
+    title: 'Settings',
+    path: '/settings',
+    icon: <SettingOutlined />
+  }
+]
