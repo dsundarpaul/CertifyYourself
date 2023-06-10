d53400897c2428e9ace613d09e80c4ea298f6141
AnujS21 <anuj.sherekar10@gmail.com>
2023-06-10 22:18:32 +0530

    Added user profile edit form in drawer

--- src/views/UserScreens/UserProfile/UserProfileEdit/UserProfileEdit.jsx
+++ src/views/UserScreens/UserProfile/UserProfileEdit/UserProfileEdit.jsx
@@ -1,4 +1,7 @@
-import { Form, Input } from 'antd'
+import { Button, Card, Drawer, FloatButton, Form, Input, InputNumber } from 'antd'
+import ButtonGroup from 'antd/es/button/button-group'
+import PickerButton from 'antd/es/date-picker/PickerButton'
+import FormItem from 'antd/es/form/FormItem'
 import React from 'react'
 
 const UserProfileEdit = () => {
@@ -8,7 +11,24 @@ const UserProfileEdit = () => {
         <Form.Item>
           <Input placeholder="Username" />
         </Form.Item>
+        <FormItem>
+          <Input placeholder="State" />
+        </FormItem>
+        <FormItem>
+          <Input placeholder="Country" />
+        </FormItem>
+        <FormItem>
+          <Input placeholder="School/Uni" />
+        </FormItem>
+        <FormItem>
+          <Input type='Integer' placeholder='Phone' />
+        </FormItem>
       </Form>
+      <div>
+        <Button onClick={() => {navigate('/')}}>Save</Button>
+        <Button onClick={() => {navigate('/')}}>cancel</Button>
+      </div>
+  
     </div>
   )
 }
