ed6e51c6ef1e73fc1f47502a5b6b9b15011ffb3d
dsundarpaul <dsundar0211@gmail.com>
2023-06-14 13:37:08 +0530

    improved delete and edit button's Ui in community

--- src/views/UserScreens/Community/CommunityFeeds/Feed/Feed.js
+++ src/views/UserScreens/Community/CommunityFeeds/Feed/Feed.js
@@ -1,7 +1,12 @@
-import { Button, Card } from "antd";
+import { Button, Card, Popconfirm } from "antd";
 import React from "react";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+
 const Feed = ({ author, feedTitle, feedContent }) => {
+
+  const handleDeleteConfirm = () => {
+    
+  }
   
   return (
     <Card
@@ -9,12 +14,19 @@ const Feed = ({ author, feedTitle, feedContent }) => {
       title={feedTitle}
       extra={
         author ? (
-          <div>
-          <Button  className=""
-            icon={<DeleteOutlined />}
-          />
-          <Button icon={ <EditOutlined />}/>
-      
+          <div className="ml-4">
+            <Popconfirm
+              title="Delete your feed"
+              description="Are you sure to delete this feed?"
+              icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+              onConfirm={handleDeleteConfirm}
+              // onCancel={cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button  className="mr-2" danger icon={<DeleteOutlined />} />
+            </Popconfirm>
+            <Button icon={ <EditOutlined />}/>
           </div>
         ) : (
           <></>
