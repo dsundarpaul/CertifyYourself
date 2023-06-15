1716f76f93cb74468481fc1ea270ae04913e2a2b
vivekkale9 <vivekskale03@gmail.com>
2023-06-15 22:29:55 +0530

    feed edit

--- src/views/UserScreens/Community/CommunityFeeds/Feed/Feed.js
+++ src/views/UserScreens/Community/CommunityFeeds/Feed/Feed.js
@@ -1,13 +1,23 @@
 import { Button, Card, Popconfirm } from "antd";
 import React from "react";
-import { DeleteOutlined, EditOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
+import { useNavigate } from "react-router";
+import { kebabCase } from "lodash";
 
 const Feed = ({ author, isAuthor, feedTitle, feedContent }) => {
+  const navigate = useNavigate();
+  const handleDeleteConfirm = () => {};
 
-  const handleDeleteConfirm = () => {
-    
-  }
-  
+  const navigateTo = () => {
+    console.log("helllooo")
+    const communitFeed_uid = encodeURI(kebabCase(feedTitle));
+    navigate(`/feed-edit/${communitFeed_uid}`);
+  };
+
   return (
     <Card
       className="border-2"
@@ -18,23 +28,22 @@ const Feed = ({ author, isAuthor, feedTitle, feedContent }) => {
             <Popconfirm
               title="Delete your feed"
               description="Are you sure to delete this feed?"
-              icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+              icon={<QuestionCircleOutlined style={{ color: "red" }} />}
               onConfirm={handleDeleteConfirm}
               // onCancel={cancel}
               okText="Yes"
               cancelText="No"
             >
-              <Button  className="mr-2" danger icon={<DeleteOutlined />} />
+              <Button className="mr-2" danger icon={<DeleteOutlined />} />
             </Popconfirm>
-            <Button icon={ <EditOutlined />}/>
+            <Button onClick={navigateTo()} icon={<EditOutlined />} />
           </div>
         ) : (
           <></>
         )
       }
     >
-      {author} : 
-      {feedContent}
+      {author} :{feedContent}
     </Card>
   );
 };
