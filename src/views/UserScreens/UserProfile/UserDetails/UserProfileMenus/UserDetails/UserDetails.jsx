dc28ea35e52c9d1fd174bf93ddb744b52a84e222
vivekkale9 <vivekskale03@gmail.com>
2023-06-20 22:22:40 +0530

    user profile changes

--- src/views/UserScreens/UserProfile/UserDetails/UserProfileMenus/UserDetails/UserDetails.jsx
+++ src/views/UserScreens/UserProfile/UserDetails/UserProfileMenus/UserDetails/UserDetails.jsx
@@ -15,9 +15,11 @@ const UserDetails = () => {
     useUserDetails(getDetails, user?.uid);
   }, [user?.uid]);
 
+  
+
   return (
     <div>
-      <Card className="bg-yellow-100">
+      <Card className="bg-yellow-100" >
         <Typography.Title level={3}>{userDetails?.userName}</Typography.Title>
         {userDetails?.rank || "unranked"}
 
@@ -28,6 +30,12 @@ const UserDetails = () => {
         <Typography>Last Visit</Typography>
 
         <Typography>Registered</Typography>
+
+        <Typography>{userDetails.userCountry}</Typography>
+        <Typography>{userDetails.userName}</Typography>
+        <Typography>{userDetails.userPhoneNumber}</Typography>
+        <Typography>{userDetails.userSchoolOrUni}</Typography>
+        <Typography>{userDetails.userState}</Typography>
       </Card>
     </div>
   );
