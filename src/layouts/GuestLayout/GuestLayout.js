e19c65eb06c0ccaf04a8678803ae9bd2bd9ceab8
dsundarpaul <109967226+dsundarpaul@users.noreply.github.com>
2023-09-09 03:03:45 +0530

    added gradiant to guest Layout

--- src/layouts/GuestLayout/GuestLayout.js
+++ src/layouts/GuestLayout/GuestLayout.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 // import { Layout } from 'antd'
-import './GuestLayoutStyles.css'
+import "./GuestLayoutStyles.css";
 
 // const Content
 const GuestLayout = ({ children }) => {
   return (
     // <Layout.Content>
-      <div className='guest-layout-header'>
-        { children }
-      </div>
+    // <div className="guest-layout-header bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r">
+    <div className="guest-layout-header bg-gradient-to-r from-gray-100 to-gray-300">
+      {children}
+    </div>
     // </Layout.Content>
-  )
-}
+  );
+};
 
-export default GuestLayout
+export default GuestLayout;
